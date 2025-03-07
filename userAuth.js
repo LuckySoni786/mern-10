67c66b9c80baf8d6c1a84fc2d85511821722c5e8
Lucky <luckysoni9782@gmail.com>
2025-03-07 15:39:41 +0530

    set auth

---
+++ userAuth.js
@@ -0,0 +1,9 @@
+let password = prompt("enter your password :");
+if(password.length >=8){
+    console.log("storong Password");
+    
+}
+else{
+    console.log("please set strong password");
+    
+}
