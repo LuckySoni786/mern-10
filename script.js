5eca149deec2d49dbef015c9186d22418f896ed3
Lucky <luckysoni9782@gmail.com>
2025-03-07 14:40:04 +0530

    changeddd

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let a = 20;
-console.log(a);
+// let a = 20;
+// console.log(a);
  
-function add(){
-    console.log("git hub");
+// function add(){
+//     console.log("git hub");
+    
+// }
+
+let a = Number(prompt("enter number :"));
+function add(a,b){
+    console.log(a + b);
     
 }
+add(a,a);
